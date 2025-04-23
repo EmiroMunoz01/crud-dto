21a0ec93205b177cf5890450ea47cc1b1ff83108
EmiroMunoz01 <emiro_2001@icloud.com>
2025-04-23 11:13:32 -0500

    Consideraciones importantes en la entidad, designando campos que deben ser unicos

--- src/main/java/com/security/cruddto/modelo/EntidadUsuario.java
+++ src/main/java/com/security/cruddto/modelo/EntidadUsuario.java
@@ -1,10 +1,7 @@
 package com.security.cruddto.modelo;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -17,21 +14,34 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "entidad_usuario")
 public class EntidadUsuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "nombre", nullable = false, unique = false)
     @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
 
+
+    @Column(name = "direccion", nullable = false, unique = false)
+    @NotBlank(message = "La direccion es obligatorio")
+    private String direccion;
+
+
+    @Column(name = "cedula", nullable = false, unique = true)
     @NotNull(message = "El precio es obligatorio")
     private Long cedula;
 
+
+    @Column(name = "email", nullable = false, unique = true)
     @NotBlank(message = "El email es obligatorio")
     private String email;
 
+
+    @Column(name = "clave", nullable = false, unique = false)
     @NotBlank(message = "La clave es obligatoria")
     private String clave;
 }
--- src/main/java/com/security/cruddto/servicio/UsuarioServicio.java
+++ src/main/java/com/security/cruddto/servicio/UsuarioServicio.java
@@ -18,27 +18,35 @@ public class UsuarioServicio implements IUsuario {
         this.repositorio = repositorio;
     }
 
-
     @Override
     public List<EntidadUsuario> listarUsuario() {
-        return List.of();
+        return repositorio.findAll();
     }
 
+
+    //Metodo para eliminar un usuario mediante email
     @Override
     public void eliminarUsuarioPorEmail(String email) {
         repositorio.deleteByEmail(email);
     }
 
+
+
+    //Metodo para crear un usuario
     @Override
     public EntidadUsuario crearUsuario(EntidadUsuario usuario) {
-        return null;
+        return repositorio.save(usuario);
     }
 
+
+    //Metodo para actualizar un usuario
     @Override
     public EntidadUsuario actualizarUsuarioPorEmail(String email, EntidadUsuario usuario) {
         return null;
     }
 
+
+    //Metodo para eliminar un usuario
     @Override
     public EntidadUsuario buscarUsuarioPorEmail(String email) {
         return null;
